ab6ddb7dca819b569a890e027e9edc9af73269bf
noobpk <ltp.noobpk@gmail.com>
2025-10-16 22:59:53 +0700

    Update AppConfig to load additional configuration from environment variables

--- helpers/config.py
+++ helpers/config.py
@@ -10,5 +10,9 @@ class AppConfig(object):
     """Base configuration class."""
 
     logo_path: Path = Path(__file__).parent.parent / "assets" / "logo-2.png"
+    icon_path: Path = Path(__file__).parent.parent / "assets" / "logo.png"
     favicon_path: Path = Path(__file__).parent.parent / "assets" / "favicon.ico"
-    page_title: str = "Cell "
+    app_name: str = str(os.getenv("APP_NAME", "Cell"))
+    page_title: str = str(os.getenv("PAGE_TITLE", "Cell - GenAI Chat UI"))
+    log_max_size: int = int(os.getenv("LOG_MAX_SIZE", 10000000))  # in bytes
+    log_max_backups: int = int(os.getenv("LOG_MAX_BACKUPS", 5))  # number of backup files
