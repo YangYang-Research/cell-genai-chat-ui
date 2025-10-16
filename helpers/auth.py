5a22746d0b47d42ba99956fe9746f952cf718592
noobpk <ltp.noobpk@gmail.com>
2025-10-16 22:59:41 +0700

    Add authentication configuration and login functionality

---
+++ helpers/auth.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import jwt
+from typing import Optional, Dict
+import extra_streamlit_components as stx
+from helpers.config import AppConfig
+
+config = AppConfig()
+
+cookie_manager = stx.CookieManager()
+
+def verify_jwt_cookie(jwt_cookie: str) -> Optional[Dict]:
+    try:
+        payload = jwt.decode(jwt_cookie, "your_secret_key", algorithms=["HS256"])
+        return payload
+    except jwt.ExpiredSignatureError:
+        st.warning("Session expired. Please log in again.")
+        return None
+    except jwt.InvalidTokenError as e:
+        st.error("Invalid token. Please log in again.")
+        return None
+    
+def clear_jwt_cookie(cookie_name: str):
+    cookie_manager.delete(cookie_name)
+
+def get_logout():
+    st.session_state["authentication_status"] = None
+    clear_jwt_cookie("cell")
+
+def get_user_info(extend_key: str) -> Optional[Dict]:
+    all_cookies = cookie_manager.get_all(key="cookie-"+extend_key)
+
+    if 'cell' in all_cookies:
+        jwt_cookie = all_cookies['cell']
+    
+    if st.session_state.get("authentication_status"):
+        user_info = st.session_state.get("username")
+        if not user_info:
+            user_info = verify_jwt_cookie(jwt_cookie)
+        return user_info
+    elif jwt_cookie:
+        user_info = verify_jwt_cookie(jwt_cookie)
+        if user_info:
+            return user_info
+    else:
+        clear_jwt_cookie("cell")
+        return None
---
+++ pages/login.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import base64
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+from helpers.config import AppConfig
+from helpers.loog import logger
+
+with open('auth_config.yml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+stauth.Hasher.hash_passwords(config['credentials'])
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+# ------------- Login Page Class -------------
+class LoginPage:
+    def __init__(self):
+        self.config = AppConfig()
+
+    def display(self):
+        st.logo(self.config.logo_path, size="large", icon_image=self.config.logo_path)
+
+        col1, col2, col3 = st.columns([1, 2, 1])
+        with col2:
+            base64_logo = base64.b64encode(open(self.config.logo_path, "rb").read()).decode("utf-8")
+            st.markdown(
+                f"""
+                <div style="text-align: left;">
+                    <h2><img src="data:image/png;base64,{base64_logo}" alt="Logo" style="width: 100px;"/>{self.config.app_name}</h2>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+
+            authenticator.login()
+
+            if st.session_state.get("authentication_status"):
+                st.success("Login successful!")
+                st.rerun()
+            elif st.session_state.get("authentication_status") is False:
+                st.error("Username/password is incorrect")
+            else:
+                st.info("Please enter your username and password")
+
+    def run(self):
+        self.display()
+    
+def main():
+    try:
+        page = LoginPage()
+        page.run()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        st.error("An unexpected error occurred. Please refresh the page and try again.")
+        st.exception(e)
+
+if __name__ == "__main__":
+    main()
