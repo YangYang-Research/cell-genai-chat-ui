60f99ae7a495b69a443cc835aee4c670731a6898
noobpk <ltp.noobpk@gmail.com>
2025-10-21 23:01:37 +0700

    Add CellHTTP class for handling chat requests with streaming support

---
+++ helpers/http.py
@@ -0,0 +1,40 @@
+import requests
+from helpers.config import AppConfig, AWSConfig, ChatConfig
+from helpers.secret import AWSSecretManager
+from helpers.loog import logger
+
+class CellHTTP:
+    def __init__(self, app_conf: AppConfig, aws_conf: AWSConfig, chat_conf: ChatConfig):
+        self.app_conf = app_conf
+        self.aws_conf = aws_conf
+        self.chat_conf = chat_conf
+        self.aws_secret_manager = AWSSecretManager(app_conf, aws_conf)
+
+    def stream_chat_request(self, prompt, history):
+        """
+        Stream tokens from backend API (StreamingResponse).
+        """
+        payload = {
+            "model_id": self.chat_conf.chat_model_id,
+            "temperature": self.chat_conf.temperature,
+            "max_tokens": self.chat_conf.max_response_tokens,
+            "messages": [
+                {"role": "user" if m.type == "human" else "assistant", "content": m.content}
+                for m in history.messages
+            ] + [{"role": "user", "content": prompt}],
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.aws_secret_manager.get_secret(self.chat_conf.chat_auth_key_name)}",
+        }
+        print("DEBUG PAYLOAD:", payload)
+        try:
+            with requests.post(self.chat_conf.chat_service_api+"chat/completions", headers=headers, json=payload, stream=True, timeout=self.chat_conf.chat_timeout_seconds) as r:
+                r.raise_for_status()
+                for chunk in r.iter_content(chunk_size=None):
+                    if chunk:
+                        yield chunk.decode("utf-8")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"[FE-CHAT_SERVICE] Stream error: {e}")
+            yield f"\n[Error] Unable connect to chat service. Please try again."
