8068e912a9e22beb312f114ae7ef340beff0d138
noobpk <ltp.noobpk@gmail.com>
2025-10-29 23:37:18 +0700

    refactor http.py

--- helpers/http.py
+++ helpers/http.py
@@ -1,7 +1,9 @@
 import requests
-from helpers.config import AppConfig, AWSConfig, ChatConfig
-from helpers.secret import AWSSecretManager
+import streamlit as st
 from helpers.loog import logger
+from helpers.secret import AWSSecretManager
+from helpers.utils import Utils
+from helpers.config import AppConfig, AWSConfig, ChatConfig
 
 class MakeRequest(object):
     def __init__(self, app_conf: AppConfig, aws_conf: AWSConfig, chat_conf: ChatConfig):
@@ -10,39 +12,92 @@ class MakeRequest(object):
         self.chat_conf = chat_conf
         self.aws_secret_manager = AWSSecretManager(app_conf, aws_conf)
 
-    def stream_chat_completions(self, prompt: str, history: dict, attachments: list):
+    def stream_chat_completions(self, chat_model: str, history: dict, prompt: str, attachments: list):
         """
         Stream tokens from backend API (StreamingResponse).
         """
 
         messages = []
-        if self.chat_conf.chat_model_name == "claude":
-            # Convert chat history to Claude-compatible messages
+
+        if chat_model == "claude":
             messages = [
                     {"role": "user" if m.type == "human" else "assistant", "content": m.content}
                     for m in history.messages
-                ] # + [{"role": "user", "content": prompt}]
-            
-            # if attachments:
-            #     for attachment in attachments:
-            #         if attachment.status == "completed":
-            #             if attachment.is_image and attachment.base64:
-
-            #             elif attachment.is_document and attachment.bytes:
-                            
-            #             elif attachment.is_text and attachment.content:
-
-            #             else:
-            #                 pass
+                ]
+        
+            if attachments:
+                for attachment in attachments:
+                    if attachment.status.value == "completed":
+                        if attachment.is_image and attachment.base64:
+                            messages = messages + [
+                                {
+                                    "role": "user", 
+                                    "content": [
+                                        {
+                                            "type": "text", 
+                                            "text": prompt
+                                        },
+                                        {
+                                            "type": "image",
+                                            "source": {
+                                                "type": "base64",
+                                                "media_type": attachment.type,
+                                                "data": attachment.base64,
+                                            },
+                                        },
+                                    ]
+                                }
+                            ]
+                        elif attachment.is_document and attachment.base64:
+                            messages = messages + [
+                                {
+                                    "role": "user", 
+                                    "content": [
+                                        {
+                                            "type": "text", 
+                                            "text": prompt
+                                        },
+                                        {
+                                            "document": {
+                                                # Available formats: html, md, pdf, doc/docx, xls/xlsx, csv, and txt
+                                                "format": Utils.get_file_format(attachment.type),
+                                                "name": Utils.format_filename(attachment.name),
+                                                "source": {"bytes": attachment.base64}, #(convert bytes → base64 string) for sending over HTTP
+                                            }
+                                        },
+                                    ]
+                                }
+                            ]
+                        elif attachment.is_text and attachment.content:
+                            messages = messages + [
+                                {
+                                    "role": "user", 
+                                    "content": [
+                                        {
+                                            "type": "text", 
+                                            "text": prompt
+                                        },
+                                        {
+                                            "type": "text", 
+                                            "text": attachment.content
+                                        },
+                                    ]
+                                }
+                            ]
+                        else:
+                            pass
+            else:
+                messages = messages + [{"role": "user", "content": prompt}]
+        else:
+            st.toast(f"Model : {chat_model} currently not supported", icon="⚠️")
+            st.stop()
 
         if isinstance(messages, tuple):
             messages = list(messages)
 
         payload = {
-            "model_id": self.chat_conf.chat_model_id,
-            "model_name": self.chat_conf.chat_model_name,
-            "temperature": self.chat_conf.temperature,
-            "max_tokens": self.chat_conf.max_response_tokens,
+            "chat_session_id": "123",
+            "model_name": chat_model,
             "messages": messages,
         }
 
